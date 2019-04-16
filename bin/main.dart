69ff29408d24363234e0ad161012fd407cc169d8
Phạm Như Vũ <phamnhuvu.dev@gmail.com>
2019-04-16 10:48:16 +0700

    init commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_template_cli/flutter_template_cli.dart' as flutter_template_cli;
+
+main(List<String> arguments) {
+  print('Hello world: ${flutter_template_cli.calculate()}!');
+}
---
+++ test/flutter_template_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_template_cli/flutter_template_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
