87579617d4a812c6a40a601503de83bd690b899b
Phạm Như Vũ <phamnhuvu.dev@gmail.com>
2019-04-16 16:31:03 +0700

    template for widget stateless and statefull

--- bin/flutter_template_cli.dart
+++ bin/flutter_template_cli.dart
@@ -14,13 +14,13 @@ main(List<String> arguments) {
   print('Hello world: ${flutter_template_cli.calculate()}!');
   print(Directory.current.path);
 
-  final parser = ArgParser()
+  final parser = new ArgParser()
     ..addOption(name, defaultsTo: "", abbr: "n")
     ..addFlag(widget, negatable: false, abbr: "w")
     ..addFlag(state, negatable: false, abbr: "s");
   argResults = parser.parse(arguments);
 
   if (argResults[widget]) {
-    buildWidget(name: argResults[name], isStateful: argResults[state] as bool);
+    buildWidget(name: argResults[name], isStateful: argResults[state]);
   }
 }
