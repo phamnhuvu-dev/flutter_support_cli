21ab9705fa482eee6449869995fccfda2bfb1253
Phạm Như Vũ <phamnhuvu.dev@gmail.com>
2019-04-16 11:14:00 +0700

    test run command line in project

--- bin/flutter_template_cli.dart
+++ bin/flutter_template_cli.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_template_cli/flutter_template_cli.dart' as flutter_template_cli;
+import 'dart:io';
+
 
 main(List<String> arguments) {
   print('Hello world: ${flutter_template_cli.calculate()}!');
+  print(Directory.current.path);
 }
